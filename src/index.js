b72ef72f4c8dd0007e0c986f9edb9133820c3957
calvin96818-gif <calvin96818@gmail.com>
2026-02-15 19:03:19 +0800

    Create src/index.js as the main entry point for React application

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Main entry point for React application
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
