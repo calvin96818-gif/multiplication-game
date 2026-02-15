5ea94f1ca5962d1142b07161a28f00f955ac7903
calvin96818-gif <calvin96818@gmail.com>
2026-02-15 21:01:43 +0800

    Create Home.jsx component for class joining interface

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Join the Multiplication Game!</h1>
+            <p>Welcome to the multiplication game. Here you can join and start your adventure in learning multiplication!</p>
+            <button>Join Game</button>
+        </div>
+    );
+};
+
+export default Home;
